e4d057cef9360d485d6b2599a6cc8cf53a9d3868
reypanda1 <157523262+reypanda1@users.noreply.github.com>
2025-11-03 20:19:52 -0500

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -1,16 +1,23 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local function OnItemUse(source, item)
+local function OnItemUse(source, item, imagePath)
     local Player = QBCore.Functions.GetPlayer(source)
     
     if not Player then
         return
     end
     
-    TriggerClientEvent('RAB_maps:showImage', source, nil)
+    TriggerClientEvent('RAB_maps:showImage', source, imagePath)
 end
 
-QBCore.Functions.CreateUseableItem(Config.ItemName, OnItemUse)
-
-print(string.format("^2[RAB_MAPS]^7 Item '%s' registrado correctamente", Config.ItemName))
+if Config.Items and next(Config.Items) then
+    local itemCount = 0
+    for itemName, imagePath in pairs(Config.Items) do
+        QBCore.Functions.CreateUseableItem(itemName, function(source, item)
+            OnItemUse(source, item, imagePath)
+        end)
+        itemCount = itemCount + 1
+    end
+    print(string.format("^2[RAB_MAPS]^7 %d item(s) registrado(s) correctamente", itemCount))
+end
 
